a78f8889bba8af3453c3a9db6d10eed0f20cc0bd
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-21 16:14:19 -0300

    update: formatted podcast episodes data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { parseISO, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  thumbnail: string;
+  description: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 };
 
 type HomeProps = {
@@ -28,9 +37,26 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
   return {
     props: {
-      episodes: data,
+      episodes: episodes,
     },
     revalidate: 60 * 60 * 8, // 8 hours
   };
