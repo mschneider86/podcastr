a28e17c76fab7df24169ba1824c64f00c9cb6a47
Matheus Schneider <matheus.ssm@gmail.com>
2021-04-22 10:40:38 -0300

    updated player style and added slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
+import Image from 'next/image';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export function Player() {
   const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
@@ -11,18 +14,39 @@ export function Player() {
     <div className={styles.playerContainer}>
       <header>
         <img src='/playing.svg' alt='Tocando agora' />
-        <strong>Tocando agora {episode?.title}</strong>
+        <strong>Tocando agora </strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={192}
+            height={192}
+            src={episode.thumbnail}
+            objectFit='cover'
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
 
-      <footer className={styles.empty}>
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#84d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#84d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
